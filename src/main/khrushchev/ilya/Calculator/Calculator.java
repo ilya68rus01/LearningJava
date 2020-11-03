cdc544c24295f5a80bce56bdf5c29b0badf122bd
Ilya <ilya68rus01@gmail.com>
2020-11-03 18:54:52 +0300

    Complete Calculator v0.1

--- src/main/khrushchev/ilya/Calculator/Calculator.java
+++ src/main/khrushchev/ilya/Calculator/Calculator.java
@@ -1,9 +1,7 @@
 package khrushchev.ilya.Calculator;
 
-import com.sun.deploy.cache.BaseLocalApplicationProperties;
-import com.sun.org.glassfish.gmbal.Description;
-import khrushchev.ilya.Factorial;
 
+import com.sun.org.glassfish.gmbal.Description;
 import java.util.*;
 import java.util.function.BinaryOperator;
 import java.util.regex.Matcher;
@@ -14,16 +12,22 @@ import static java.lang.Double.parseDouble;
 public class Calculator {
     class Parser {
         Pattern bracketsPattern;
+        Pattern multAndDivisionPattern;
+        Pattern addAndSubsPattern;
 
 
         Parser(){
             // TODO Починить регулярку чтобы она находила скобки с /
-            String brackets = "\\((\\d(\\.\\d+)?)([\\+|\\-|\\*|\\/]{1})(\\d(\\.\\d+)?)\\)";
+            String brackets = "\\((\\-?\\d+(\\.\\d+)?)([\\+|\\-|\\*|\\/]{1})(\\-?\\d+(\\.\\d+)?)\\)";
+            String multAndDivision = "([\\+|\\-]?\\d+(\\.\\d+)?)(\\*|\\/){1}(\\-?\\d+(\\.\\d+)?)";
+            String addAndSub = "(\\-?\\d+(\\.\\d+)?)(\\+|\\-){1}(\\d+(\\.\\d+)?)";
             bracketsPattern = Pattern.compile(brackets);
+            multAndDivisionPattern = Pattern.compile(multAndDivision);
+            addAndSubsPattern = Pattern.compile(addAndSub);
         }
 
-        @Description("Method for parse string with ( ) and return Object[number1,operation,number2]")
-        Object[] parse(String str){
+//        @Description("Method for parse string with ( ) and return Object[number1,operation,number2]")
+        String parse(String str){
             double x1=0.0, x2=0.0;
             String operation="";
             String resultString = str;
@@ -36,11 +40,31 @@ public class Calculator {
                 resultString = resultString.replace(bracketsMatcher.group(0),result.toString());
                 System.out.println(resultString);
             }
-            // TODO Реализовать остальную часть парсера для простой строки resultString формата a+b-c*x/y
+            Matcher mdMatcher = multAndDivisionPattern.matcher(resultString);
+            while (mdMatcher.find()){
+                x1 = parseDouble(mdMatcher.group(1));
+                operation = mdMatcher.group(3);
+                x2 = parseDouble(mdMatcher.group(4));
+                Double result = dictionary.get(operation).apply(x1,x2);
+                resultString = resultString.replace(mdMatcher.group(), result>0? "+"+result.toString():result.toString());
+                mdMatcher = multAndDivisionPattern.matcher(resultString);
+                System.out.println(resultString);
+            }
+            Matcher asMatcher = addAndSubsPattern.matcher(resultString);
+            while (asMatcher.find()){
+                x1 = parseDouble(asMatcher.group(1));
+                operation = asMatcher.group(3);
+                x2 = parseDouble(asMatcher.group(4));
+                Double result = dictionary.get(operation).apply(x1,x2);
+                resultString = resultString.replace(asMatcher.group(),result.toString());
+                asMatcher = addAndSubsPattern.matcher(resultString);
+                System.out.println(resultString);
+            }
+            // (\d(\.\d+)?)[\*|\/]{1}(\d(\.\d+)?)
             // \(\d(\.\d+)?[\+|\-|\*|\/]{1}\d(\.\d+)?\)
             // 1-(2.21+1.42)+(3-1.5)-(1.5*2)+(43/21.254) = 1+3.63+1.5+3+...
-//            return resultString;
-            return new Object[]{x1,operation,x2};
+            return resultString;
+//            return new Object[]{x1,operation,x2};
         }
     }
 
@@ -57,14 +81,15 @@ public class Calculator {
 
     public String calculate(String expression){
         Double resultValue;
-        Object[] params = parser.parse(expression);
-        resultValue = dictionary.get(params[1].toString()).apply((double)params[0],(double)params[2]);
-        return resultValue.toString();
+//        Object[] params = parser.parse(expression);
+//        resultValue = dictionary.get(params[1].toString()).apply((double)params[0],(double)params[2]);
+//        return resultValue.toString();
+        return parser.parse(expression);
     }
 
     public static void main(String[] args) {
         Calculator calc = new Calculator();
-        String sr = "1-(2.21+1.42)+(3-1.5)-(1.5*2)";
+        String sr = "1-(2.21+1.42)*(-3-1.5)+9/(-1.5/2)+(3*2)";
         String value = calc.calculate(sr);
         System.out.println(value);
     }
