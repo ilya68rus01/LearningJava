09fd658ad821911650b07ed4329170078dec7c30
Ilya <ilya68rus01@gmail.com>
2020-11-04 17:52:16 +0300

    Complete calculator

--- src/main/khrushchev/ilya/Calculator/Calculator.java
+++ src/main/khrushchev/ilya/Calculator/Calculator.java
@@ -1,7 +1,8 @@
 package khrushchev.ilya.Calculator;
 
 
-import com.sun.org.glassfish.gmbal.Description;
+import com.sun.istack.internal.NotNull;
+
 import java.util.*;
 import java.util.function.BinaryOperator;
 import java.util.regex.Matcher;
@@ -15,63 +16,52 @@ public class Calculator {
         Pattern multAndDivisionPattern;
         Pattern addAndSubsPattern;
 
-
         Parser(){
-            // TODO Починить регулярку чтобы она находила скобки с /
             String brackets = "\\((\\-?\\d+(\\.\\d+)?)([\\+|\\-|\\*|\\/]{1})(\\-?\\d+(\\.\\d+)?)\\)";
             String multAndDivision = "([\\+|\\-]?\\d+(\\.\\d+)?)(\\*|\\/){1}(\\-?\\d+(\\.\\d+)?)";
-            String addAndSub = "(\\-?\\d+(\\.\\d+)?)(\\+|\\-){1}(\\d+(\\.\\d+)?)";
+            String addAndSub = "([\\+|\\-]?\\d+(\\.\\d+)?)(\\+|\\-){1}(\\d+(\\.\\d+)?)";
             bracketsPattern = Pattern.compile(brackets);
             multAndDivisionPattern = Pattern.compile(multAndDivision);
             addAndSubsPattern = Pattern.compile(addAndSub);
         }
 
-//        @Description("Method for parse string with ( ) and return Object[number1,operation,number2]")
-        String parse(String str){
-            double x1=0.0, x2=0.0;
-            String operation="";
+        Float parse(String str){
             String resultString = str;
             Matcher bracketsMatcher = bracketsPattern.matcher(resultString);
             while (bracketsMatcher.find()) {
-                x1 = parseDouble(bracketsMatcher.group(1)); // Возвращает левый (первый) операнд
-                operation = bracketsMatcher.group(3); // Возвращает операцию
-                x2 = parseDouble(bracketsMatcher.group(4)); // Возвращает правый (второй) операнд
-                Double result = dictionary.get(operation).apply(x1,x2);
-                resultString = resultString.replace(bracketsMatcher.group(0),result.toString());
-                System.out.println(resultString);
+                resultString = resultString.replace(bracketsMatcher.group(0),
+                        simplifyExpression(bracketsMatcher));
+                bracketsMatcher = bracketsPattern.matcher(resultString);
             }
-            Matcher mdMatcher = multAndDivisionPattern.matcher(resultString);
-            while (mdMatcher.find()){
-                x1 = parseDouble(mdMatcher.group(1));
-                operation = mdMatcher.group(3);
-                x2 = parseDouble(mdMatcher.group(4));
-                Double result = dictionary.get(operation).apply(x1,x2);
-                resultString = resultString.replace(mdMatcher.group(), result>0? "+"+result.toString():result.toString());
-                mdMatcher = multAndDivisionPattern.matcher(resultString);
-                System.out.println(resultString);
+            Matcher multiplyAndDivisionMatcher = multAndDivisionPattern.matcher(resultString);
+            while (multiplyAndDivisionMatcher.find()){
+                Double result = parseDouble(simplifyExpression(multiplyAndDivisionMatcher));
+                resultString = resultString.replace(multiplyAndDivisionMatcher.group(0),
+                        result>0? "+"+result.toString():result.toString());
+                multiplyAndDivisionMatcher = multAndDivisionPattern.matcher(resultString);
             }
-            Matcher asMatcher = addAndSubsPattern.matcher(resultString);
-            while (asMatcher.find()){
-                x1 = parseDouble(asMatcher.group(1));
-                operation = asMatcher.group(3);
-                x2 = parseDouble(asMatcher.group(4));
-                Double result = dictionary.get(operation).apply(x1,x2);
-                resultString = resultString.replace(asMatcher.group(),result.toString());
-                asMatcher = addAndSubsPattern.matcher(resultString);
-                System.out.println(resultString);
+            Matcher addictionAndSubstractMatcher = addAndSubsPattern.matcher(resultString);
+            while (addictionAndSubstractMatcher.find()){
+                resultString = resultString.replace(addictionAndSubstractMatcher.group(0),
+                        simplifyExpression(addictionAndSubstractMatcher));
+                addictionAndSubstractMatcher = addAndSubsPattern.matcher(resultString);
             }
-            // (\d(\.\d+)?)[\*|\/]{1}(\d(\.\d+)?)
-            // \(\d(\.\d+)?[\+|\-|\*|\/]{1}\d(\.\d+)?\)
-            // 1-(2.21+1.42)+(3-1.5)-(1.5*2)+(43/21.254) = 1+3.63+1.5+3+...
-            return resultString;
-//            return new Object[]{x1,operation,x2};
+            return Float.parseFloat(resultString);
+        }
+
+        private String simplifyExpression(@NotNull Matcher matcher){
+            double x1 = parseDouble(matcher.group(1)); // Возвращает левый (первый) операнд
+            String operation = matcher.group(3); // Возвращает операцию
+            double x2 = parseDouble(matcher.group(4)); // Возвращает правый (второй) операнд
+            Double result = dictionary.get(operation).apply(x1,x2);
+            return result.toString();
         }
     }
 
     private Parser parser;
     private Map<String, BinaryOperator<Double>> dictionary = new HashMap<String,BinaryOperator<Double>>();
 
-    Calculator(){
+    public Calculator(){
         dictionary.put("+", (x1, x2) -> x1+x2);
         dictionary.put("-", (x1, x2) -> x1-x2);
         dictionary.put("*", (x1, x2) -> x1*x2);
@@ -80,18 +70,7 @@ public class Calculator {
     }
 
     public String calculate(String expression){
-        Double resultValue;
-//        Object[] params = parser.parse(expression);
-//        resultValue = dictionary.get(params[1].toString()).apply((double)params[0],(double)params[2]);
-//        return resultValue.toString();
-        return parser.parse(expression);
+        Float result = parser.parse(expression);
+        return result.toString();
     }
-
-    public static void main(String[] args) {
-        Calculator calc = new Calculator();
-        String sr = "1-(2.21+1.42)*(-3-1.5)+9/(-1.5/2)+(3*2)";
-        String value = calc.calculate(sr);
-        System.out.println(value);
-    }
-
 }
--- src/main/khrushchev/ilya/Main.java
+++ src/main/khrushchev/ilya/Main.java
@@ -1,13 +1,14 @@
 package khrushchev.ilya;
 
+import khrushchev.ilya.Calculator.Calculator;
 import khrushchev.ilya.NumberSequence.*;
 
 public class Main {
 
     public static void main(String[] args) {
-       Factorial factorial = new Factorial();
-       int value = factorial.getFactorial(5);
-       System.out.println(value);
+        Calculator calc = new Calculator();
+        String result = calc.calculate("3.125*12-(18/9)+4/(0.05+1.95)-137.5");
+        System.out.println(result);
     }
 }
 
